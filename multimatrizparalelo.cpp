04d907edc6894e167a649995c5e84b75b7f7d5c0
Yarid Perez <ayperezc@unal.edu.co>
2019-04-17 20:24:31 -0500

    disminuyendo comentarios

--- multimatrizparalelo.cpp
+++ multimatrizparalelo.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <pthread.h>
 #include <iomanip>
@@ -5,66 +6,97 @@
 
 using namespace std;
 
-#define NUM_THREADS 16 // 2,4,8,16
+// maximum size of matrix 
+#define MAX 1024 
+  
+// maximum number of threads 
+#define NUM_THREADS 2 
+  
+long matA[MAX][MAX]; 
+long matB[MAX][MAX]; 
+long matC[MAX][MAX]; 
+int paso = 0;
 
 struct entradas {
-    double acum;
     long inicio;
     long fin;
 };
 
-void* gregLeibParalelo(void* args){  //OJO!!!!!, esto da pi/4
+void* multi(void* args) 
+{ 
     struct entradas *misEntradas;
     misEntradas = (struct entradas *) args;
     long inicio = misEntradas->inicio;
     long fin = misEntradas->fin;
-    double numerador,denominador,fraccion;
-    int alternante = -1;
-    
-    for(long i = inicio; i < fin; i++)
-    {
-        alternante = -alternante;
-        numerador = alternante;
-        denominador = 2*i + 1;
-        fraccion = numerador/denominador;
-        misEntradas->acum = misEntradas->acum + fraccion;
+    for(long i = inicio; i < fin; i++){
+        for (int j = 0; j < MAX; j++){
+            for (int k = 0; k < MAX; k++){
+                matC[i][j] += matA[i][k] * matB[k][j];
+            }
+        }
     }
-
     pthread_exit(NULL);
-}
+} 
 
-int main (void) {
+int main(void){
+
     pthread_t threads[NUM_THREADS];
     struct entradas td[NUM_THREADS];
-    void *status;
-    long paso = 1000000000/NUM_THREADS;
-    double acumTotal = 0;
+    long paso = MAX/NUM_THREADS;
+
+    int k;
+    for(int i = 0; i < MAX; i++)
+    {
+        k=1;
+        for(int j = 0; j < MAX; j++)
+        {
+            matA[i][j] = k;
+            k++;
+        }
+        
+    }
+
+    for(int i = 0; i < MAX; i++)
+    {
+        k=1;
+        for(int j = 0; j < MAX; j++)
+        {
+            matB[i][j] = k;
+            k++;
+        }
+        
+    }
+    
 
     clock_t t;
     t = clock();
-
     for (int i = 0; i < NUM_THREADS; ++i)
     {
         long imas1 = i+1;
         td[i].inicio = i*paso;
         td[i].fin = imas1*paso;
-        int t = pthread_create(&threads[i], NULL, gregLeibParalelo, (void *)&td[i]);
+        int t = pthread_create(&threads[i], NULL, multi, (void *)&td[i]);
         if (t != 0)
         {
             cout << "Error in thread creation: " << t << endl;
         }
     }
-    for(int i = 0; i < NUM_THREADS; ++i)
-    {
-        int rc = pthread_join(threads[i], &status);
+    for (int i = 0; i < NUM_THREADS; ++i){
+        int rc = pthread_join(threads[i], NULL);
         if (rc) {
             cout << "Error:unable to join," << rc << endl;
             exit(-1);
         }
-        acumTotal = acumTotal + td[i].acum;
     }
-    
-    cout << std::setprecision(15) << 4*acumTotal << endl;
     t = clock() - t;
     printf ("It took me %d clicks (%f seconds).\n",(int)t,((float)t)/CLOCKS_PER_SEC);
+
+    // Displaying the result matrix 
+    // cout << endl 
+    //      << "Multiplication of A and B" << endl; 
+    // for (int i = 0; i < MAX; i++) { 
+    //     for (int j = 0; j < MAX; j++)  
+    //         cout << matC[i][j] << " ";         
+    //     cout << endl; 
+    // } 
 }
